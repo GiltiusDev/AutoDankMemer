6d1949c8f79a9ffbe56e36ca7bb8e8701bb4f4a9
GiltiusDev <87497894+GiltiusDev@users.noreply.github.com>
2021-09-01 22:20:56 +0200

    Add files via upload

---
+++ phone.py
@@ -0,0 +1,12 @@
+import pyautogui as auto
+import time
+
+time.sleep(10)
+while True:
+    auto.write("pls fish")
+    auto.press("enter")
+    auto.write("pls dig")
+    auto.press("enter")
+    auto.write("pls hunt")
+    auto.press("enter")
+    time.sleep(41)
